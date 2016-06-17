c977fa5d7fb036dec722dcd3d853044c28a92f21
zhangtian <zhangtian@happy>
2016-06-17 20:01:36 +0800

    注解升级替代

--- spring4-boot-security-web/src/main/java/cn/itcast/security/WebSecurityConfig.java
+++ spring4-boot-security-web/src/main/java/cn/itcast/security/WebSecurityConfig.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 
@@ -20,7 +20,7 @@ import cn.itcast.security.service.CustomUserDetailsService;
  * 配置Spring Security
  */
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
